7fa086c9eb7d2fd79ebdda66bb68fb7846e4e6c9
ZettaiVR <zettaivr@gmail.com>
2020-08-25 03:20:53 +0200

    Add VRC.Core.RemoteConfig.GetString and .Init to improve compatibility with Thry editor (used by the Poiyomi shaders)

---
+++ Sources/VRCCore-Editor/RemoteConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VRC.Core
+{
+    public class RemoteConfig
+    {
+        public static string GetString(string _) { return _; }
+        public static void Init(Action _) { }
+    }
+}
