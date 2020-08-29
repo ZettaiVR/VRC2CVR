456380fd696ab81894d903cdbc9b024c3b0e0718
ZettaiVR <zettaivr@gmail.com>
2020-08-29 02:31:36 +0200

    Add VRC_SpatialAudioSource component

--- Sources/VRCSDK2/VRC_IkFollower.cs
+++ Sources/VRCSDK2/VRC_IkFollower.cs
@@ -4,6 +4,6 @@ namespace VRCSDK2
 {
     public class VRC_IKFollower : MonoBehaviour
     {
-        
+
     }
 }
---
+++ Sources/VRCSDK2/VRC_SpatialAudioSource.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace VRCSDK2
+{
+    [ExecuteInEditMode]
+    [RequireComponent(typeof(AudioSource))]
+    [DisallowMultipleComponent]
+    public class VRC_SpatialAudioSource : MonoBehaviour
+    {
+        public float Gain = 10f;
+        public float Far = 40f;
+        public bool EnableSpatialization = true;
+        public float Near;
+        public float VolumetricRadius;
+        public bool UseAudioSourceVolumeCurve;
+    }
+}
